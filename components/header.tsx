d1804c81d3f2714efd2ffac1b3360e190914027e
8295557345 <aakash.chopra95@gmail.com>
2024-02-13 01:06:30 -0500

    Project Section added

--- components/header.tsx
+++ components/header.tsx
@@ -1,21 +1,14 @@
 "use client";
 
-import React from 'react'
+import React, { useState } from 'react'
 import { motion } from 'framer-motion';
 import {links} from '@/lib/data'
 import Link from 'next/link';
+import clsx from 'clsx';
 
 export default function Header() {
+  const [activeSection, setActiveSection] = useState('Home');
   return (
-    // <header className="z-999 relative">
-    // <motion.div className="fixed top-0 left-1/2 -translate-x-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40
-    //     bg-white bg-opacity-80 shadow-lg shadow-black\[0.3] backdrop-blur-[0.5rem] sm:top-6 
-    //     sm:h-3.25rem sm:w-36rem sm:rounded-full" 
-    //     initial={{y:-100 ,opacity: 0}} 
-    //     animate={{y:0, opacity:1}}>
-      
-    // </motion.div>
-
     <header className="z-[999] relative">
       <motion.div
         className="fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75"
@@ -32,10 +25,26 @@ export default function Header() {
               initial={{ y: -100, opacity: 0 }}
               animate={{ y: 0, opacity: 1 }}
             >
-                <Link className='flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:text-gray-500 dark:hover:text-gray-30' 
-                        href={link.hash}>
-                    {link.name}
+                <Link 
+                  className={clsx('flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition', {
+                      'dark:text-gray-500 dark:hover:text-gray-30': true, // Always apply this class
+                      'text-gray-950': activeSection === link.name // Apply this class conditionally
+                  })} 
+                  href={link.hash}
+                  onClick={()=>setActiveSection(link.name)}>
+                      {link.name}
+                    {
+                      link.name === activeSection && (
+                        <motion.span className='bg-gray-100 rounded-full absolute inset-0 -z-10' layoutId='activeSection' transition={{
+                          type: "spring",
+                          stiffness: 380,
+                          damping: 30
+                        }}></motion.span>
+                      )
+                    }
+                   
                 </Link>
+
             </motion.li>
             ))}
     </ul>
