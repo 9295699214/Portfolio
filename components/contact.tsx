baa8a10376dd265985aa8dce5e6bfb58f10e981f
9295699214 <aakash.chopra95@gmail.com>
2024-02-18 21:28:55 -0500

    Footer and Contact form cempleted

--- components/contact.tsx
+++ components/contact.tsx
@@ -1,46 +1,74 @@
-import React from 'react'
-import SectionHeading from './section-heading'
-// import { sendEmail } from "@/actions/sendEmail";
+"use client";
+
+import React from "react";
+import SectionHeading from "./section-heading";
+import { motion } from "framer-motion";
+import { useSectionInView } from "@/lib/hooks";
+import { sendEmail } from "@/actions/sendEmail";
 import SubmitBtn from "./submit-btn";
 import toast from "react-hot-toast";
 
 export default function Contact() {
+  const { ref } = useSectionInView("Contact");
+
   return (
-    <div>
-        <section id="contact" className='mb-2 sm:mb:-28 w-[min(100%,38rem)] text-center'>
-            <SectionHeading>Contact me</SectionHeading>
-            <p className='text-hgray-700 -mt-5'>Please contact me directly at <a className='underline' href="mailto:aakash.chopra95@gmail.com">aakash.chopra95@gmail.com</a> or through this form</p>
-            <form
-              className="mt-10 flex flex-col dark:text-black"
-              // action={async (formData) => {
-              //   const { data, error } = await sendEmail(formData);
+    <motion.section
+      id="contact"
+      ref={ref}
+      className="mb-20 sm:mb-28 w-[min(100%,38rem)] text-center"
+      initial={{
+        opacity: 0,
+      }}
+      whileInView={{
+        opacity: 1,
+      }}
+      transition={{
+        duration: 1,
+      }}
+      viewport={{
+        once: true,
+      }}
+    >
+      <SectionHeading>Contact me</SectionHeading>
 
-              //   if (error) {
-              //     toast.error(error);
-              //     return;
-              //   }
+      <p className="text-gray-700 -mt-6 dark:text-white/80">
+        Please contact me directly at{" "}
+        <a className="underline" href="mailto:example@gmail.com">
+          example@gmail.com
+        </a>{" "}
+        or through this form.
+      </p>
 
-              //   toast.success("Email sent successfully!");
-              // }}
-            >
-              <input
-                className="h-14 px-4 rounded-lg borderBlack dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none"
-                name="senderEmail"
-                type="email"
-                required
-                maxLength={500}
-                placeholder="Your email"
-              />
-              <textarea
-                className="h-52 my-3 rounded-lg borderBlack p-4 dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none"
-                name="message"
-                placeholder="Your message"
-                required
-                maxLength={5000}
-              />
-              <SubmitBtn />
+      <form
+        className="mt-10 flex flex-col dark:text-black"
+        action={async (formData) => {
+          const { data, error } = await sendEmail(formData);
+
+          if (error) {
+            toast.error(error);
+            return;
+          }
+
+          toast.success("Email sent successfully!");
+        }}
+      >
+        <input
+          className="h-14 px-4 rounded-lg borderBlack dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none"
+          name="senderEmail"
+          type="email"
+          required
+          maxLength={500}
+          placeholder="Your email"
+        />
+        <textarea
+          className="h-52 my-3 rounded-lg borderBlack p-4 dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none"
+          name="message"
+          placeholder="Your message"
+          required
+          maxLength={5000}
+        />
+        <SubmitBtn />
       </form>
-        </section>
-    </div>
-  )
+    </motion.section>
+  );
 }
--- components/experience.tsx
+++ components/experience.tsx
@@ -31,15 +31,14 @@ export default function Experience() {
             <VerticalTimelineElement
               contentStyle={{
                 visibility: "visible",
-                background: "#f3f4f6",
+                background:"#f3f4f6",
                 boxShadow: "none",
                 border: "1px solid rgba(0, 0, 0, 0.05)",
                 textAlign: "left",
                 padding: "1.3rem 2rem",
               }}
               contentArrowStyle={{
-                borderRight: "0.4rem solid #9ca3af",
-                   
+                borderRight: "0.4rem solid #9ca3af"
               }}
               date={item.date}
               icon={item.icon}
@@ -50,7 +49,6 @@ export default function Experience() {
             >
               <h3 className="font-semibold capitalize">{item.title}</h3>
               <p className="font-normal !mt-0">{item.location}</p>
-              <p className="font-normal !mt-0">{item.company}</p>
               <p className="!mt-1 !font-normal text-gray-700 dark:text-white/75">
                 {item.description}
               </p>
